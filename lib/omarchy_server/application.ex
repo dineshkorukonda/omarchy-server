3277f615fd4240f712313e5602ba1e392938fb24
dineshkorukonda <dineshkorukonda05@gmail.com>
2026-09-03 10:52:51 +0530

    feat(daemon): scaffold Elixir/OTP application skeleton

---
+++ lib/omarchy_server/application.ex
@@ -0,0 +1,13 @@
+defmodule OmarchyServer.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = []
+
+    opts = [strategy: :one_for_one, name: OmarchyServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
